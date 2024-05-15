30e9539939fef26ab936d6a6a36ad27157f0b180
Marko Ristic <marko.ristic.aus@gmail.com>
2024-05-15 15:51:46 +1000

    Query parser implemented - Chemical clauses are aggregate functions and used in HAVING after conditions are grouped - Condition clauses are their presence in a subquery list Changed parser to allow chemical id not to be specified (e.g. can search by ph only) Chemical name search removed (from a UI the id will always be known) Query type definitions updated Validation for non simple types added (e.g. chemical must be specified by at least one of id, conc/units, ph) Disallowed additional parameters in the query objects as recursive matching was having problems since some classes can have all types as None and get matched always

--- api/screens.py
+++ api/screens.py
@@ -2,11 +2,16 @@
 
 """
 
-from sqlmodel import Session, select, or_
+from sqlmodel import Session, select, or_, and_, case, col, func
 from fastapi import APIRouter, Depends
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from pydantic.functional_validators import model_validator
 import api.db as db
+
+# ============================================================================ #
+# Screen search query type model
+# ============================================================================ #
 
 class LogicExpOp(str, Enum):
     and_ = "and"
@@ -14,91 +19,136 @@ class LogicExpOp(str, Enum):
 
 class ChemicalArg(BaseModel):
     id: int | None
-    name: str | None
     conc: float | None
     units: str | None
     ph: float | None
+    # To help with mathcing, don't allow extra params that might make another class match
+    model_config = ConfigDict(extra='forbid')
+    # Check some form of chemical identification is provided
+    @model_validator(mode='after')
+    def check_id_or_conc_or_ph(self):
+        if self.id == None and self.conc == None and self.ph == None:
+            raise ValueError("Must specify chemical with at least one of id, concentration and unit, or ph!")
+        if not((self.conc == None and self.units == None) or (self.conc != None and self.units != None)):
+            raise ValueError("Chemical concentration and units must be either both or neither present!")
+        return self
 
 class ChemicalLogicExp(BaseModel):
     op: LogicExpOp
     arg_left: "ChemicalLogicExp | ChemicalArg"
     arg_right: "ChemicalLogicExp | ChemicalArg"
+    model_config = ConfigDict(extra='forbid')
 
 class WellConditionArg(BaseModel):
     id: int | None = None
     include_similar: bool = False
     chems: ChemicalLogicExp | ChemicalArg | None = None
+    # To help with mathcing, don't allow extra params that might make another class match
+    model_config = ConfigDict(extra='forbid')
+    # Check that chemical information isn't provided if condition id is
+    @model_validator(mode='after')
+    def check_id_or_chemicals(self):
+        if (self.id == None and self.chems == None) or (self.id != None and self.chems != None):
+            raise ValueError("Must specify condition exclusively by either an id or by chemicals!")
+        return self
 
 class WellConditionLogicExp(BaseModel):
     op: LogicExpOp
     arg_left: "WellConditionLogicExp | WellConditionArg"
     arg_right: "WellConditionLogicExp | WellConditionArg"
+    model_config = ConfigDict(extra='forbid')
 
 class ScreenQuery(BaseModel):
     name: str | None = None
     only_available: bool = False
     conds: WellConditionLogicExp | WellConditionArg | None = None
+    # To help with mathcing, don't allow extra params that might make another class match
+    model_config = ConfigDict(extra='forbid')
+    # Check that some form of screen identification is provided for query
+    @model_validator(mode='after')
+    def check_name_or_conditions(self):
+        if self.name == None and self.conds == None:
+            raise ValueError("Must specify screens by at least a name or by conditions!")
+        return self
+
+# Rebuild the classes with self reference (not sure if this is makes a difference)
+ChemicalLogicExp.model_rebuild()
+WellConditionLogicExp.model_rebuild()
+
+# ============================================================================ #
+# Screen search query parser
+# ============================================================================ #
 
 def parseQuery(query: ScreenQuery):
-    params = []
-    parseWellConditionLogicExp(query.conds, params)
+    # Create clause tree for conditions
+    if type(query.conds) == WellConditionLogicExp:
+        clauses = parseWellConditionLogicExp(query.conds)
+    else:
+        clauses = parseWellConditionArg(query.conds)
+    # Final statement groups by screen
+    screens = select(db.Screen).join(db.Well).join(db.WellCondition).where(clauses).group_by(db.Screen)
+    return screens
 
-def parseWellConditionLogicExp(condexp: WellConditionLogicExp, clauses):
+def parseWellConditionLogicExp(condexp: WellConditionLogicExp):
+    # Look at both sides of operator and add sub-clauses to list
     sub_clauses = []
     for arg in [condexp.arg_left, condexp.arg_right]:
         if type(arg) == WellConditionLogicExp:
-            parseWellConditionLogicExp(arg, sub_clauses)
+            sub_clauses.append(parseWellConditionLogicExp(arg))
         else:
-            parseWellConditionArg(arg, sub_clauses)
+            sub_clauses.append(parseWellConditionArg(arg))
+    # Combine sub-clauses with operator
     if condexp.op == LogicExpOp.or_:
-        clauses.append(or_(sub_clauses))
+        clause = or_(*sub_clauses)
     else:
-        clauses += sub_clauses
-    return clauses
+        clause = and_(*sub_clauses)
+    return clause
 
-def parseWellConditionArg(cond: WellConditionArg, clauses):
+def parseWellConditionArg(cond: WellConditionArg):
+    # Selecting a specific well condition id
+    conditions = select(db.WellCondition.id)
+    # Either captured by an id
     if cond.id != None:
-        clauses.append(db.WellCondition.id == cond.id) # match all screens where
+        conditions = conditions.where(db.WellCondition.id == cond.id)
+    # Or by chemical information
     elif cond.chems != None:
-        sub_clauses = []
         if type(cond.chems) == ChemicalLogicExp:
-            parseChemicalLogicExp(cond.chems, sub_clauses)
+            clauses = parseChemicalLogicExp(cond.chems)
         else:
-            parseChemicalArg(cond.chems, sub_clauses)
-        clauses.append(sub_clauses)
-    return clauses
+            clauses = parseChemicalArg(cond.chems)
+        # Possible conditions are grouped and filtered by HAVING clause on chemical information
+        conditions = conditions.join(db.WellCondition_Factor_Link).join(db.Factor).join(db.Chemical).group_by(db.WellCondition.id).having(clauses)
+    # Condition expression is checking if the condition is in the produced list
+    condition_sub_clause = col(db.WellCondition.id).in_(conditions)
+    return condition_sub_clause
 
-def parseChemicalLogicExp(chemexp: ChemicalLogicExp, params):
-    if chemexp.op == LogicExpOp.and_:
-        if type(chemexp.arg_left) == ChemicalLogicExp:
-            parseChemicalLogicExp(chemexp.arg_left, params)
+def parseChemicalLogicExp(chemexp: ChemicalLogicExp):
+    # Look at both sides of the operator and add sub-clauses to list
+    sub_clauses = []
+    for arg in [chemexp.arg_left, chemexp.arg_right]:
+        if type(arg) == ChemicalLogicExp:
+            sub_clauses.append(parseChemicalLogicExp(arg))
         else:
-            parseChemicalArg(chemexp.arg_left, params)
+            sub_clauses.append(parseChemicalArg(arg))
+    # Combine sub-clauses with operator
+    if chemexp.op == LogicExpOp.or_:
+        clause = or_(*sub_clauses)
     else:
-        lparams=[]
-        if type(chemexp.arg_left) == ChemicalLogicExp:
-            parseChemicalLogicExp(chemexp.arg_left, lparams)
-        else:
-            parseChemicalArg(chemexp.arg_left, lparams)
-        rparams = []
-        if type(chemexp.arg_right) == ChemicalLogicExp:
-            parseChemicalLogicExp(chemexp.arg_right, rparams)
-        else:
-            parseChemicalArg(chemexp.arg_right, rparams)
-        params.append(or_(lparams, rparams))
-    return params
+        clause = and_(*sub_clauses)
+    return clause
 
-def parseChemicalArg(chem: ChemicalArg, params):
+def parseChemicalArg(chem: ChemicalArg):
+    # Selecting a chemical by id, conc/units or ph (or any combination of them)
+    match_conditions = True
     if chem.id != None:
-        params.append(db.Chemical.id == chem.id)
-    elif chem.name != None:
-        params.append(db.Chemical.name == chem.name)
+        match_conditions = match_conditions and db.Chemical.id == chem.id
     if chem.conc != None and chem.units != None:
-        params.append(db.Chemical.conc == chem.conc)
-        params.append(db.Chemical.units == chem.units)
+        match_conditions = match_conditions and db.Chemical.conc == chem.conc and db.Chemical.units == chem.units
     if chem.ph != None:
-        params.append(db.Chemical.ph == chem.ph)
-    return params
+        match_conditions = match_conditions and db.Chemical.ph == chem.ph
+    # Chemical expression is true if the selected chemical is present in any factor for a condition (used after GROUP BY)
+    literal = func.max(case((match_conditions, 1), else_=0))==1
+    return literal
 
 # ============================================================================ #
 # API operations
@@ -129,8 +179,8 @@ async def get_screens_query(*, session: Session=Depends(db.get_session), query: 
     """
     Get a list of screens filtered by query parameters
     """
-    statement = select(db.Screen)
     # Parse Query here
+    statement = parseQuery(query)
     screens = session.exec(statement).all()
     return screens
 
