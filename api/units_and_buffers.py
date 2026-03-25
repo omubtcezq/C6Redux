85d0ffa40668306a3da0fcea40b1f9b208dab1aa
David Saifer <spyrotheultimate@gmail.com>
2026-03-25 11:00:20 -0700

    change var name to be more understandable and add shotgun submodule

--- api/units_and_buffers.py
+++ api/units_and_buffers.py
@@ -103,14 +103,11 @@ def condition_equality(condition1: db.WellCondition, condition2: db.WellConditio
         return True
     # the allowable difference for factor a ph differences (just a very small number in case of floating point issues)
     epsilon = 1.01
-    # make factors > 1 so our later multiplying makes sense
     if len(condition1.factors) != len(condition2.factors):
         return False
-    
     for factor1 in condition1.factors:
         conc1 = unit_conversion(factor1.concentration, factor1.unit, factor1.chemical.density, factor1.chemical.molecular_weight, "M")
-        # makes sure one is equal
-        flag = False
+        any_equal = False
         for factor2 in condition2.factors:
             if factor1.chemical_id != factor2.chemical_id:
                 continue
@@ -124,8 +121,8 @@ def condition_equality(condition1: db.WellCondition, condition2: db.WellConditio
             # make sure the concentration was properly converted first
             if conc1 is None or conc2 is None or conc1 * epsilon < conc2 or conc1 / epsilon > conc2 :
                 continue
-            flag = True
-        if not flag:
+            any_equal = True
+        if not any_equal:
             return False
             
     return True
